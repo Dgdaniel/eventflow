4de1cb101c721a787e5380b3d0d0f217da94a701
d@niz-daniel <danielamegninou170@gmail.com>
2026-04-27 21:16:51 +0000

    feat(tickets): add Kafka and Database modules to TicketsService and update package.json with new event service start scripts

--- apps/tickets-service/src/tickets-service.module.ts
+++ apps/tickets-service/src/tickets-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { KafkaModule } from '@app/kafka';
+import { DatabaseModule } from '@app/database';
 import { TicketsServiceController } from './tickets-service.controller';
 import { TicketsServiceService } from './tickets-service.service';
 
 @Module({
-  imports: [],
+  imports: [KafkaModule.register('tickets-service-group'), DatabaseModule],
   controllers: [TicketsServiceController],
   providers: [TicketsServiceService],
 })
